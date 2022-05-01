ed1d2108d6264507ada5f8d55c52e7e62013126c
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-01 15:39:35 +0430

    complete heroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <section className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        {/* <Image src="d" alt="headphones" className="hero-banner-image" /> */}
+
+        <div>
+          <Link href={`/products/ID`} passHref>
+            <a>
+              <button type="button">BUTTON TEXT</button>
+            </a>
+          </Link>
+
+          <div className="desc">
+            <h5>Description</h5>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
+
       <section className="products-heading">
         <h2>Beset Selling Products</h2>
         <p>Speakers of may variations</p>
       </section>
+
       <section className="products-container">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((product) => product)}
       </section>
-      <footer></footer>
+
+      <FooterBanner></FooterBanner>
     </>
   );
 };
