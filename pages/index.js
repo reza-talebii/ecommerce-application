8efc9888ab5e9a21edcff9673203c8bf42395f5f
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-01 15:03:58 +0430

    create componets fle

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <section>
+      <section className="products-heading">
         <h2>Beset Selling Products</h2>
         <p>Speakers of may variations</p>
       </section>
-      <section>
+      <section className="products-container">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((product) => product)}
       </section>
       <footer></footer>
