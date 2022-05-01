01392a2ec7ed542652cb568028319388823ce0d2
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-01 14:56:41 +0430

    add classes style

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <section>
+        <h2>Beset Selling Products</h2>
+        <p>Speakers of may variations</p>
+      </section>
+      <section>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((product) => product)}
+      </section>
+      <footer></footer>
+    </>
+  );
 };
 
-export default index;
+export default Home;
